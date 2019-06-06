0a33274ddfa86d19227c21dad3b9f942aaa56dc4
csamiselo <chisambwe.samiselo@gmail.com>
2019-06-06 17:12:06 +0200

    Program to demo call by value

---
+++ PassByvalue.java
@@ -0,0 +1,33 @@
+/*
+A simple Java Program to demonstrate what passing an argument to a subroutine 
+using call by value method.This approach copies the value of an argument into the formal
+parameter of the subroutine. Therefore, changes made to the parameter of the subroutine
+have no effect on the argument
+In Java passing a primitive type to a method passes it by value
+*/
+
+class Origin {
+    void method(int x, int y) {
+        x *= 6;
+        y /= 6;
+    }
+}
+
+class PassByvalue {
+
+    public static void main(String[] args) {
+        Origin og = new Origin();
+       int  x = 12;
+        int y = 30;
+        // displays primitive values before the method call is invoked
+        System.out.println("Values of  x and y before method call :" + x + " " + y);
+
+        // Method invoked
+        og.method(x, y);
+
+        // displays primitive values after the method call is invoked
+
+        System.out.println("Values of  x and y before method call :" + x + " " + y);
+    }
+
+}
