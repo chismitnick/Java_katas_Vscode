99d27b278d46ecfda42914562f89505df3181baa
csamiselo <chisambwe.samiselo@gmail.com>
2019-06-12 16:52:28 +0200

    Update

--- AcessControl.java
+++ AcessControl.java
@@ -2,9 +2,8 @@
  A simple program to demonstrate Acess control specifiers 
  private and public
  */
-
  class Base {
-    int a; // default access modifier for Javav is public
+    int a; // default access modifier for Java is public
     public int b;
     private int  c;// can only be accessed through getter and setter methods 
     
@@ -16,18 +15,18 @@
      }
  }
 
- class AcessControl {
-
-
+class AcessControl {
 public static void main(String[] args) {
     Base ob  = new Base();
 
     //Access a nd b directly 
+    //becuase they are 
     ob.a = 14;
     ob.b = 16;
     
     ob.setc(100);
-
+    
+    // Print the values of a,b and c 
     System.out.println("The values of a,b and c are :" + ob.a +" "+ ob.b + " " + ob.getc());
 }
 
